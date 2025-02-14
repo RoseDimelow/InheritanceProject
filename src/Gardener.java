9163961e2fd7114a8b60b5bacc411e1c40ff204f
mac <mac@macs-mbp-6.t-mobile.com>
2025-02-13 18:03:23 -0700

    mwahahahahaha

--- src/Gardener.java
+++ src/Gardener.java
@@ -1,8 +1,11 @@
 
 public class Gardener {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 
+		System.out.println("Well hello there...");
+		System.out.println("General Kenobi!");
 		
 	}
 
