4d42900dbfdcbf67a4c242f69930e8833870cda8
rose.dimelow <rose.dimelow@MU-C1-22.mullen.pvt.k12.co.us>
2025-02-18 13:29:29 -0700

    heyyyyyyyyyyyyyyy

--- src/Gardener.java
+++ src/Gardener.java
@@ -4,10 +4,7 @@ public class Gardener {
 	public static void main(String[] args) 
 	{
 
-		System.out.println("Well hello there...");
-		System.out.println("General Kenobi!");
-		//hi
-		//hi
+		
 		
 	}
 
